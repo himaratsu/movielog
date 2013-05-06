51fd64d1ac0002f53d81e779144a5e99a3c06493
himara2 <himaratsu@gmail.com>
2013-05-07 08:17:08 +0900

    add follow/unfollow func with validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,15 +20,24 @@ class UsersController < ApplicationController
   def follow
     @my_num = 1 #いずれセッションを使う
     @user = User.find(params[:id])
-    @friend = Friend.new(:following_id => @my_num, :followed_id => params[:id])
-    @friend.save
-    redirect_to user_path(params[:id]), notice:  'フォロー成功！'
+    @is_friend = Friend.where('following_id = ? and followed_id = ?', @my_num, @user.id).count
+    if @is_friend == 0
+      @friend = Friend.new(:following_id => @my_num, :followed_id => params[:id])
+      @friend.save
+      redirect_to user_path(params[:id]), notice:  'フォロー成功！'
+    else
+      redirect_to user_path(params[:id]), notice: 'すでにフォローしています'
+    end
   end
 
   def unfollow
     @my_num = 1 #いずれセッションを使う
     @friend = Friend.where('following_id = ? and followed_id = ?', @my_num, params[:id]).first
-    @friend.destroy
-    redirect_to user_path(params[:id]), notice: 'フォロー解除！'
+    if @friend.present?
+      @friend.destroy
+      redirect_to user_path(params[:id]), notice: 'フォロー解除！'
+    else
+      redirect_to user_path(params[:id]), notice: 'フォローしていないユーザです'
+    end
   end
 end
